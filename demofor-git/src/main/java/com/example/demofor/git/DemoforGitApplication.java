2e84357fa321973c9e7e689a3dde0107bf6e5d38
NITHIN K BHAT <76273749+NITHIN-K-BHAT@users.noreply.github.com>
2023-12-20 18:28:08 +0530

    Update DemoforGitApplication.java

--- demofor-git/src/main/java/com/example/demofor/git/DemoforGitApplication.java
+++ demofor-git/src/main/java/com/example/demofor/git/DemoforGitApplication.java
@@ -10,6 +10,7 @@ public class DemoforGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoforGitApplication.class, args);
 
+		//Fibanaci Series
 		int n1=0,n2=1,n3,i,count=10;
 		System.out.print(n1+" "+n2);//printing 0 and 1
 
